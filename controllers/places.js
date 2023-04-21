732ce055c9e4fcfcc791377dbeeaefdc7af7f792
Ryanhu-hub <ryanhuot20@gmail.com>
2023-04-20 22:12:39 -0700

    changed places.js in models to placesArray.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,42 +1,28 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
+const router = require("express").Router();
+const places = require("../models/placesArray.js");
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/rest_place.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/rest_food.jpg'
-      }]
-      
-    res.render('places/index', {places})
-})
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  // console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = "Anytown";
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = "USA";
   }
-  places.push(req.body)
-  res.redirect('/places')
-})
+  places.push(req.body);
+  res.redirect("/places");
+});
 
-
-module.exports = router
+module.exports = router;
--- views/error404.jsx
+++ views/error404.jsx
@@ -1,19 +1,19 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-              <div>
-                <img src="/images/sad_cat.jpg" alt="Sad Cat"/>
-                </div>
-          </main>
-      </Def>
-    )
-  }
-  
+function error404() {
+  return (
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry, we can't find this page!</p>
+        <a href="/">Click Here to go Home</a>
+        <div>
+          <img src="/images/sad_cat.jpg" alt="Sad Cat" />
+        </div>
+      </main>
+    </Def>
+  );
+}
 
-module.exports = error404
+module.exports = error404;
